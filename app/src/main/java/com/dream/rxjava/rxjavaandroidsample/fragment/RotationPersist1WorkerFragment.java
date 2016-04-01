09f77d3aee9af25b82a8352f9cf04561c05b262c
susong0618 <751971697@qq.com>
2016-04-01 18:20:10 +0800

    RxJavaAndroidSample

---
+++ app/src/main/java/com/dream/rxjava/rxjavaandroidsample/fragment/RotationPersist1WorkerFragment.java
@@ -0,0 +1,11 @@
+package com.dream.rxjava.rxjavaandroidsample.fragment;
+
+/**
+ * Author:      SuSong
+ * Email:       751971697@qq.com | susong0618@163.com
+ * GitHub:      https://github.com/susong0618
+ * Date:        16/4/1 下午6:19
+ * Description: RxJavaDemo
+ */
+public class RotationPersist1WorkerFragment {
+}
